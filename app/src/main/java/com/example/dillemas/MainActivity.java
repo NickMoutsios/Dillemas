661cfb0c73161c873832f4fdc87fd616b5ab5327
Moutsiovas <moutsiosvasileios@gmail.com>
2016-12-07 00:59:13 +0200

    Major changes in background and xml attributes

--- app/src/main/java/com/example/dillemas/MainActivity.java
+++ app/src/main/java/com/example/dillemas/MainActivity.java
@@ -5,12 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544~3347511713");
+
+       // AdView mAdView = (AdView) findViewById(R.id.adView);
+      //  AdRequest adRequest = new AdRequest.Builder().build();
+       // mAdView.loadAd(adRequest);
     }
 
     //Moving to next activity.
